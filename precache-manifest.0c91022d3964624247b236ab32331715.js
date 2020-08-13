5ba38462b83bd7ec750c41c75c1699e6b75c98ee
NeupaneKhemRaj <shekharneupane@gmail.com>
2020-08-13 19:04:52 +0300

    Updates

---
+++ precache-manifest.0c91022d3964624247b236ab32331715.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1c90f61056bccf74efd84e82e9fe9db",
+    "url": "/e-commerce-redux-react/index.html"
+  },
+  {
+    "revision": "2bd76d02b316a0685078",
+    "url": "/e-commerce-redux-react/static/css/main.cbfb6e08.chunk.css"
+  },
+  {
+    "revision": "de25f51ee4ea2b568cee",
+    "url": "/e-commerce-redux-react/static/js/2.80f49256.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/e-commerce-redux-react/static/js/2.80f49256.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bd76d02b316a0685078",
+    "url": "/e-commerce-redux-react/static/js/main.4b07f1a4.chunk.js"
+  },
+  {
+    "revision": "8418b5670a902393e3af",
+    "url": "/e-commerce-redux-react/static/js/runtime-main.882eb883.js"
+  }
+]);
